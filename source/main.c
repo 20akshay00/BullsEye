c05bea0d9ccccb5866d50861f8dea5b9a737f68c
Akshay <sakshays.2000@gmail.com>
2025-06-20 15:26:19 +0200

    clean up structs

--- source/main.c
+++ source/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <nds.h>
-#include <gl2d.h>
-
-#include <nf_lib.h>
 #include <filesystem.h>
+#include <nf_lib.h>
 
-#define TOUCH_BOX_RADIUS 4
 #define TOP_SCREEN 0
 #define BOTTOM_SCREEN 1
 
@@ -15,19 +12,27 @@ struct Vector2 {
     int y;
 };
 
-struct CrossHair{
-    int x;
-    int y;
-
+struct Sprite2D {
     int screen;
-    int sprite_id;
+    int id;
+    struct Vector2 pos; // relative to parent
+};
+
+struct CrossHair{
+    struct Sprite2D sprite;
+    struct Vector2 pos;
 };
 
 void draw(struct CrossHair *crosshair)
 {
     // Draw the crosshair sprite at the new position.
-    NF_MoveSprite(crosshair->screen, crosshair->sprite_id, crosshair->x, crosshair->y);
+    NF_MoveSprite(
+        crosshair->sprite.screen, 
+        crosshair->sprite.id, 
+        crosshair->pos.x + crosshair->sprite.pos.x, 
+        crosshair->pos.y + crosshair->sprite.pos.y);
 }
+
 
 bool readInput(uint16_t *keys_held, touchPosition *touch_pos)
 {
@@ -74,26 +79,28 @@ int main(int argc, char **argv)
     uint16_t keys_held;
     touchPosition touch_pos;
 
-    // Initialize display:
-    consoleDemoInit();
-
     // Turn on MODE 0 on the Top Screen
     NF_Set2D(TOP_SCREEN, 0);
     NF_Set2D(BOTTOM_SCREEN, 0);
-    
+    consoleDemoInit();
+
     // Set the Root Folder
     nitroFSInit(NULL);
     NF_SetRootFolder("NITROFS");
     
+    // Initialize 2D engine in both screens and use mode 0
+    NF_Set2D(TOP_SCREEN, 0);
+    NF_Set2D(BOTTOM_SCREEN, 0);
+
     // Initialize the Tiled Backgrounds System on the Top Screen
     NF_InitTiledBgBuffers();
     NF_InitTiledBgSys(TOP_SCREEN);
-    NF_InitTiledBgSys(1);       // Bottom screen
+    NF_InitTiledBgSys(BOTTOM_SCREEN);
 
     // Initialize sprite system
     NF_InitSpriteBuffers();     // Initialize storage buffers
-    NF_InitSpriteSys(TOP_SCREEN);        // Top screen
-    NF_InitSpriteSys(BOTTOM_SCREEN);        // Bottom screen
+    NF_InitSpriteSys(TOP_SCREEN);
+    NF_InitSpriteSys(BOTTOM_SCREEN);
 
     // Load the Tiled Background
     NF_LoadTiledBg("images/bg", "bg", 256, 256);
@@ -102,18 +109,34 @@ int main(int argc, char **argv)
     // Load sprite files from NitroFS
     NF_LoadSpriteGfx("images/crosshair", 0, 32, 32);
     NF_LoadSpritePal("images/crosshair", 0);
+    
+    NF_LoadSpriteGfx("images/crosshair1", 1, 32, 32);
+    NF_LoadSpritePal("images/crosshair1", 1);
 
     // Transfer the required sprites to VRAM
-    NF_VramSpriteGfx(TOP_SCREEN, 0, 0, true); // Ball: Keep all frames in VRAM
+    NF_VramSpriteGfx(TOP_SCREEN, 0, 0, true);
     NF_VramSpritePal(TOP_SCREEN, 0, 0);
 
-    // Create crosshair sprite
-    struct CrossHair crosshair;
-    crosshair.x = 0;
-    crosshair.y = 0;
-    crosshair.screen = TOP_SCREEN;
-    crosshair.sprite_id = 0;
-    NF_CreateSprite(crosshair.screen, crosshair.sprite_id, 0, 0, crosshair.x, crosshair.y);
+    NF_VramSpriteGfx(TOP_SCREEN, 1, 1, true);
+    NF_VramSpritePal(TOP_SCREEN, 1, 1);
+
+    // create cross hair
+    struct CrossHair crosshair = {
+        .pos = {0, 0},
+        .sprite = {
+            .screen = TOP_SCREEN,
+            .id = 0,
+            .pos = {0, 0}
+        }
+    };
+
+    NF_CreateSprite(
+        crosshair.sprite.screen,
+        crosshair.sprite.id,
+        0, 0,
+        crosshair.pos.x + crosshair.sprite.pos.x,
+        crosshair.pos.y + crosshair.sprite.pos.y
+    );
 
     while (1)
     {
@@ -135,8 +158,8 @@ int main(int argc, char **argv)
             shoot();
         }
 
-        crosshair.x = touch_pos.px;
-        crosshair.y = touch_pos.py;
+        crosshair.pos.x = touch_pos.px;
+        crosshair.pos.y = touch_pos.py;
         draw(&crosshair);
 
         // Update OAM array
